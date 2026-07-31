663f0f0d68b1ac5cec25963b524dc5462e5cf452
Gustavo Moraes <gustavo.correiademoraes2006@gmail.com>
2026-07-31 09:27:04 -0300

    feat(backend): register models in package

---
+++ backend/app/models/__init__.py
@@ -0,0 +1,15 @@
+from app.models.ai_analysis import AIAnalysis
+from app.models.incident import Incident
+from app.models.log_entry import LogEntry
+from app.models.notification import Notification
+from app.models.service import Service
+from app.models.user import User
+
+__all__ = [
+    "AIAnalysis",
+    "Incident",
+    "LogEntry",
+    "Notification",
+    "Service",
+    "User",
+]
